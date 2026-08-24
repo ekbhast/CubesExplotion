6b9e14bdd6b5830727d4b51d8913fbaad39fddfb
Aleksey <ekbhast@yandex.ru>
2026-08-24 22:15:06 +0500

    Настройки сцены и спавна

--- Assets/Scripts/CubeCoordinator.cs
+++ Assets/Scripts/CubeCoordinator.cs
@@ -11,8 +11,8 @@ public class CubeCoordinator : MonoBehaviour
     private float _startSplitChance = 1f;
     private List<Vector3> _startCubePositions = new();
     private CubeFactory _factory;
-    private int MaxCubeValue = 6;
-    private int MinCubeValue = 2;
+    private int MaxCubeValue = 20;
+    private int MinCubeValue = 10;
     float force = 10f;
 
 
