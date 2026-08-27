b67592349e68be1647b690472a22bd34e66bfb5a
Alexey Kirillov <ekbhast@yandex.ru>
2026-08-27 16:20:47 +0500

    refact: Теперь мы получаем ссылку на renderer один раз и далее используем ее

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,6 +5,7 @@ public class Cube : MonoBehaviour
     private Vector3 _position;
     private Vector3 _scale;
     private float _splitChance;
+    private Renderer _renderer;
 
     public Vector3 Position => _position;
     public Vector3 Scale => _scale;
@@ -20,6 +21,11 @@ public class Cube : MonoBehaviour
         _scale = scale;
         _splitChance = splitChance;
 
-        GetComponent<Renderer>().material.color = color;
+        _renderer.material.color = color;
+    }
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
     }
 }   
